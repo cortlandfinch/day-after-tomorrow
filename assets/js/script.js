22f126791b18e6c0aec9062165df7509e2e8f1d2
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-12 05:22:20 -0400

    start over script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,4 @@
 // key for weather api from OpenWeather
 var weatherKey = '4e3a40b45599c64b9dff328b5ad41a37';
-var currentDay = moment().format('L');
-var userSearchHistory = [];
 
-// function for user search using fetch to OpenWeather API for city
-function userSearchCurrent(city) {
-  var weatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${weatherKey}`;
-}
+
