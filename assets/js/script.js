23d21a86b9c74b7bb96db64ffb53764d9764e78a
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-12 05:43:00 -0400

    add form submit handler

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,25 @@
 var weatherKey = '4e3a40b45599c64b9dff328b5ad41a37';
 
 
+var citySearchEl = document.querySelector("#citySearch");
+var cityInputEl = document.querySelector("#search");
+var weatherContainerEl = document.querySelector("#weatherCity");
+var userSearchCity = document.querySelector("#city");
+
+var formSubmitHandler = function(event) {
+  // preventing page from refreshing
+  event.preventDefault();
+  // gets the value from input element
+  var search = cityInputEl.value.trim();
+
+  if (search) {
+    getUserCity(search);
+
+    // clears any old content
+    weatherContainerEl.textContent = "";
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a city name!");
+  }
+};
+
