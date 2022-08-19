28ed332d56bd28c89ee67f99ef469271e32fd3c1
cortland.finch <cortlandfinch@Cortlands-MacBook-Pro.local>
2022-08-19 11:16:45 -0400

    refactor code to match variables from script.js and remove unused html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
 // key for weather api from OpenWeather
 var weatherKey = '4e3a40b45599c64b9dff328b5ad41a37';
+var searchHistory = [];
+// url for openweather
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var searchHistoryContainer = document.querySelector('#history');
+// variable for current weather forecast
+var todayContainer = document.querySelector('#today');
+// variable for 5 day weather forecast
+var forecastContainer = document.querySelector('#forecast');
+// button to search for city
+var searchButton = document.querySelector('#search-btn');
